26e4a8af6c3e6f7bda66d7a5037eaab781a2825b
Wyatt Senalik <55900622+Lvl1Lasagna@users.noreply.github.com>
2020-08-11 17:58:19 -0500

    Added the rotation test

---
+++ Assets/Scripts/WorldObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldObj : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        FaceCamera();
+    }
+
+    private void FaceCamera()
+    {
+        Vector3 camEuler = Camera.main.transform.rotation.eulerAngles;
+        this.transform.rotation = Quaternion.Euler(camEuler.x, camEuler.y, camEuler.z);
+        UpdateSprite();
+    }
+
+    protected virtual void UpdateSprite() { }
+}
