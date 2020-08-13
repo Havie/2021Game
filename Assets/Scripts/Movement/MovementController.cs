a0012e7bacbaa9af8c8f0b550bed72022b01a3ca
Wyatt Senalik <55900622+Lvl1Lasagna@users.noreply.github.com>
2020-08-12 20:30:53 -0500

    Work on Movement also added InputController

---
+++ Assets/Scripts/Movement/MovementController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementController : MonoBehaviour
+{
+    // Reference to the camera we are using
+    private Camera _cam;
+    // Reference to the nav mesh agent on our character
+    private NavMeshAgent _agent;
+
+    [SerializeField]
+    private GameObject _testDot;
+    private bool shownPath;
+
+    // Called 0th
+    private void Awake()
+    {
+        // Set references
+        _cam = Camera.main;
+
+        try
+        {
+            _agent = this.GetComponent<NavMeshAgent>();
+        } catch
+        {
+            Debug.LogError("Could not set references in MovmentController");
+        }
+
+        shownPath = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // If the player inputted to select
+        if (InputController.GetSelectPressDown())
+        {
+            // Get the mouse's raycast world position
+            Vector3 hitPos = InputController.GetCursorRayWorldPosition();      
+            if (hitPos != Vector3.negativeInfinity)
+            {
+                // Move our agent
+                _agent.SetDestination(hitPos);
+                shownPath = false;
+            }
+        }
+
+        if (_agent.hasPath && !shownPath)
+        {
+            shownPath = true;
+            Vector3[] corners = _agent.path.corners;
+            for (int i = 0; i < corners.Length; ++i)
+            {
+                Instantiate(_testDot, corners[i], Quaternion.identity);
+            }
+        }
+    }
+}
