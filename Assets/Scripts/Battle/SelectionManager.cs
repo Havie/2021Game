b36e298b582825632f88365ab4c0af894608565e
Havie <sfdatz@live.com>
2020-08-31 13:07:26 -0500

    TryingtoAttack

--- Assets/Scripts/Battle/SelectionManager.cs
+++ Assets/Scripts/Battle/SelectionManager.cs
@@ -265,7 +265,8 @@ public class SelectionManager : MonoBehaviour
                     //verify no one else is there 
                     //Is there a way to do this from the inputcontroller? Todo?
                     RaycastHit hit;
-                    InputController.GetCursorRayWorldPosition(out hit, 1<< LayerMask.NameToLayer("Player")); // need to find layermask name/ID
+                    Debug.Log("Name to layer=" + LayerMask.NameToLayer("Player"));
+                    InputController.GetCursorRayWorldPosition(out hit, LayerMask.NameToLayer("Player")); // need to find layermask name/ID
                     //Todo Wyatt, how do we get the Laymask? bitwise shift?
                     Debug.Log("FOUND=" + hit.transform.gameObject);
                 }
