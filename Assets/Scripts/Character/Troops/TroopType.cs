2ab691042d9e25d5e36dc4874a5429cf32aad63c
Havie <sfdatz@live.com>
2020-08-31 20:20:20 -0500

    TroopChanges

---
+++ Assets/Scripts/Character/Troops/TroopType.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Troop Stats" , menuName="Troop Data/TroopDetails")]
+public class TroopType : ScriptableObject
+{
+
+    #region stats	
+    [SerializeField] private int _morale;
+    [SerializeField] private int _attack;
+    [SerializeField] private int _defense;
+    [SerializeField] private int _will;
+    [SerializeField] private int _moveSpeed;
+    [SerializeField] private int _ap;
+    #endregion
+
+
+    public int GetMorale() => _morale;
+    public int GetAttack() => _attack;
+    public int GetDefense() => _defense;
+    public int GetWill() => _will;
+    public int GetMoveSpeed() => _moveSpeed;
+    public int GetAP() => _ap;
+
+   
+
+
+}
